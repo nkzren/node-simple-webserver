8252d8bfd0ad3bd1fcaa3ecf810ff4780c80046e
nkzren <renan.nakazawa0@gmail.com>
2021-10-04 20:58:31 -0300

    adiciona navbar no header da pagina

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ app.set('view engine', 'pug');
 
 app.set('views', './views');
 
+app.use(express.static('public'));
+
 app.listen(3000, () => {
   console.log('Servidor configurado no localhost');
 });
